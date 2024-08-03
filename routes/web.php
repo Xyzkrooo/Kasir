50d9bbb7286dee451b6bb8d058bc657859fa4e48
Xyzkrooo <deas.faras@gmail.com>
2024-08-03 20:36:11 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource('/', HomeController::class);
+
